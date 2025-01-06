314f23c7ac2fcd57eb9795f7c0f9448b1ae6cd71
learnrahul29 <learnwithrahulyadav1@gmail.com>
2025-01-06 07:26:26 +0000

    conversionofdata

---
+++ basics/03conversionaloperations.js
@@ -0,0 +1,20 @@
+let score = 33
+console.log(typeof score)
+console.log(typeof(score));
+
+let valueInNumber =number(score)
+console.log(typeof valueInNumber);
+conxole.log(typeof valueInNumber);
+
+let isLoggedIn = "rahul"
+console.log(BooleanIsLoggedIn);
+let stringNumber =String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+//1 => true; 0=>false
+//" "=>false
+//"rahul"=>true
+//"33"=> 33
+//"33abc" =>nan
+//
