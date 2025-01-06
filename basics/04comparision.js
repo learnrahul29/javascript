3366f0d63964c188599b4865ccfa9bd16a7429d1
learnrahul29 <learnwithrahulyadav1@gmail.com>
2025-01-06 08:27:09 +0000

    comments

--- basics/04comparision.js
+++ basics/04comparision.js
@@ -2,13 +2,28 @@
 // console.log(2> 1);
 // console.log(2< 1);
 
-console.log(null > 0);
-console.log(null==0);
-console.log(null>=0);
+// console.log(null > 0);
+// console.log(null==0);
+// console.log(null>=0);
 
 
-console.log(undefined ==0);
-console.log(undefined >0);
-console.log(undefined <0);
-//===
-console.log("2"===2);
+// console.log(undefined ==0);
+// console.log(undefined >0);
+// console.log(undefined <0);
+// //===
+// console.log("2"===2);
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//stack (premitive ), heap (non premitive)
+let myName = "rahulyadavdotcom"
+let anotherName = myyoutube
+anothername= "chaiaurcode"
+console.log(myyoutubename);
+console.log(anothername);
+let userOne={ 
+    email: "user@gmail.com",
+    upi: "use@ybl"
+}
+userTwo.email="rahul@gmail.com"
+console.log(userOne.email);
+console.log(userTwo.email);
